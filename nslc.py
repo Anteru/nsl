9f030c509beb5459b03e4154c8de2febb86795dd
Matthäus G. Chajdas <dev@anteru.net>
2015-09-19 22:58:47 +0200

    Remove hard-coded code from the compiler driver.

--- nslc.py
+++ nslc.py
@@ -1,43 +1,14 @@
 #!/usr/bin/env python3
 from nsl.Compiler import Compiler
+import click
 
-c = Compiler ()
+@click.command()
+@click.option('--debug-parsing', default=False)
+@click.argument('input_file', type=click.File('r'))
+def Compile(input_file, debug_parsing):
+    c = Compiler ()
+    
+    c.Compile (input_file.read(), debugParsing = debug_parsing)
 
-c.Compile ('''
-struct ApplicationToVertex
-{
-    float2 position : POSITION;
-    float4 color    : UV0;
-}
-
-matrix4x4 worldViewProjection;
-Texture2D tex;
-
-struct VertexToPixel
-{
-    float4 position : POSITION;
-    float4 color    : UV0;
-}
-
-shader(vertex) (ApplicationToVertex app2vs) -> VertexToPixel
-{
-    VertexToPixel result;
-
-    result.position = worldViewProjection * float4 (app2vs.position.xy, 0, 1);
-    result.color = app2vs.color;
-
-    return result;
-}
-
-struct FragmentOutput
-{
-    float4 color : COLOR[0];
-}
-
-shader(pixel) (VertexToPixel vs2ps) -> FragmentOutput
-{
-    FragmentOutput out;
-    out.color = vs2ps.color;
-    return out;
-}
-''', debugParsing=False)
+if __name__=='__main__':
+    Compile()
