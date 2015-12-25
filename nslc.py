a93cbb394a78e0ceb78901577827c8940bcf9532
Matthäus G. Chajdas <dev@anteru.net>
2015-12-25 23:01:07 +0100

    Fix boolean options lacking the type.

--- nslc.py
+++ nslc.py
@@ -3,8 +3,8 @@ from nsl.Compiler import Compiler
 import click
 
 @click.command()
-@click.option('--debug-parsing', default=False)
-@click.option('--debug-passes', default=True)
+@click.option('--debug-parsing', default=False, type=bool)
+@click.option('--debug-passes', default=True, type=bool)
 @click.argument('input_file', type=click.File('r'))
 def Compile(input_file, debug_parsing, debug_passes):
     c = Compiler ()
