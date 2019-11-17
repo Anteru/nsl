1660028024a344b685de51fd2043b0aadc3ac1bf
Matthäus G. Chajdas <dev@anteru.net>
2019-11-17 22:34:42 +0100

    Add matrix-array access test and refactor tests.
    Use a single _compile method instead of repeating the same small code sequence.

--- tests/test_vm.py
+++ tests/test_vm.py
@@ -5,13 +5,17 @@ from nsl import (
 
 import pytest
 
-def testSimpleAddInt():
-    code = '''export function f (int a, int b) -> int { return a + b; }'''
+def _compile(code):
     c = Compiler.Compiler()
     result, ir = c.Compile(code)
     assert result == True
 
     vm = VM.VirtualMachine(ir)
+    return vm
+
+def testSimpleAddInt():
+    code = '''export function f (int a, int b) -> int { return a + b; }'''
+    vm = _compile(code)
     r = vm.Invoke('f', a=3, b=5)
     assert r == 8
 
@@ -19,11 +23,7 @@ def testSimpleAddIntToGlobal():
     code = '''int g;
     export function f (int a) -> int { return a + g; }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
     vm.SetGlobal('g', 23)
 
     r = vm.Invoke('f', a = 42)
@@ -33,11 +33,7 @@ def testSimpleAddIntToGlobalArray():
     code = '''int g[2];
     export function f (int a, int i) -> int { return a + g[i]; }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
     vm.SetGlobal('g', [5, 7])
 
     r = vm.Invoke('f', a = 3, i = 1)
@@ -47,11 +43,7 @@ def testSimpleWriteToGlobal():
     code = '''int g;
     export function f(int v) -> void { g = v; }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
     g = 0
     vm.SetGlobal('g', g)
 
@@ -67,11 +59,7 @@ def testSimpleFunctionCall():
     }
     '''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('g', a = 5)
     assert r == 10
@@ -89,11 +77,7 @@ def testOverloadedFunctionCall():
     }
     '''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('g_i', a = 5)
     assert r == 10
@@ -103,11 +87,7 @@ def testOverloadedFunctionCall():
 
 def testPrefixIncrement():
     code = '''export function f(int a) -> int { return ++a; }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', a = 1)
     assert r == 2
@@ -116,11 +96,7 @@ def testSimpleBranch():
     code = '''export function f(int a) -> int {
         if(a > 5) return 0; return 1; 
     }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', a = 0)
     assert r == 1
@@ -132,11 +108,7 @@ def testSimpleIfElseBranch():
     code = '''export function f(int a) -> int {
         if(a > 5) { return 0; } else { return 1; }
     }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', a = 0)
     assert r == 1
@@ -148,11 +120,7 @@ def testSimpleIfElseIfElseBranch():
     code = '''export function f(int a) -> int {
         if(a > 0) { return 1; } else if (a < 0) { return -1; } else { return 0; }
     }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', a = 0)
     assert r == 0
@@ -167,11 +135,7 @@ def testArrayAccessOnVector():
     code = '''export function f(float4 f, int i) -> float {
         return f[i];
     }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', f = [4, 5, 6, 7], i = 2)
     assert r == 6
@@ -180,11 +144,7 @@ def testArrayAccessOnMatrix():
     code = '''export function f(float4x4 f, int i, int j) -> float {
         return f[i][j];
     }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', f = [
         [4, 5, 6, 7],
@@ -198,11 +158,7 @@ def testConstructPrimitiveVector():
     code = '''export function f(float2 a, float b, float c) -> float4 {
         return float4(a, b, c);
     }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', a = [1, 2], b = 3, c = 4)
     assert r == [1, 2, 3, 4]
@@ -215,11 +171,7 @@ def testRunSimpleLoop():
 
         return f;
     }'''
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', f = 2, l = 4)
     assert r == 2**16
@@ -229,11 +181,7 @@ def testRunSimpleSwizzleRead():
         return p.xz;
     }'''
     
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', p = [1, 2, 3, 4])
     assert r == [1, 3]
@@ -244,11 +192,7 @@ def testRunSimpleSwizzleWrite():
         return p;
     }'''
     
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', p = [5, 6, 7, 8])
     assert r == [5, 3, 7, 8]
@@ -258,11 +202,7 @@ def testRunSimpleVectorCompare():
         return a == b;
     }'''
     
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', a = [1, 2, 3, 4], b = [1, 0, 3, 0])
     assert r == [1, 0, 1, 0]
@@ -272,11 +212,7 @@ def testRunSimpleVectorAdd():
         return a + b;
     }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', a = [1, 2, 3, 4], b = [1, 0, 3, 0])
     assert r == [2, 2, 6, 4]
@@ -291,11 +227,7 @@ def testCreateMatrix():
         );
     }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f')
     assert r == [
@@ -311,11 +243,7 @@ def testVectorTimesScalar():
     }'''
 
     
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', v=[1, 2, 3, 4], s = 2)
     assert r == [2, 4, 6, 8]
@@ -326,11 +254,7 @@ def testVectorDividedByScalar():
     }'''
 
     
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     r = vm.Invoke('f', v=[2, 4, 6, 8], s = 2)
     assert r == [1, 2, 3, 4]
@@ -340,11 +264,7 @@ def testMatrixScalarMultiply():
         return m * s;
     }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     m = [
         [1, 2, 3, 4],
@@ -360,11 +280,7 @@ def testMatrixMatrixAdd():
         return a + b;
     }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     m = [
         [1, 2, 3, 4],
@@ -380,11 +296,7 @@ def testMatrixMatrixMultiply():
         return a * b;
     }'''
 
-    c = Compiler.Compiler()
-    result, ir = c.Compile(code)
-    assert result == True
-
-    vm = VM.VirtualMachine(ir)
+    vm = _compile(code)
 
     a = [
         [5, 1, 4, 4],
@@ -406,3 +318,20 @@ def testMatrixMatrixMultiply():
         [24,  9, 25, 25],
         [58, 44, 40, 60],
     ]
+
+def testMatrixRowAccess():
+    code = '''export function f(float4x4 m, int i) -> float4 {
+        return m[i];
+    }'''
+
+    vm = _compile(code)
+
+    a = [
+        [5, 1, 4, 4],
+        [5, 5, 6, 2],
+        [4, 1, 0, 1],
+        [5, 0, 4, 4],
+    ]
+    
+    r = vm.Invoke('f', m=a, i=2)
+    assert r == [4, 1, 0, 1]
